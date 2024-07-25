d3b046fcd94b667755218f07e90240fd35a90144
newell <newell.jensen@gmail.com>
2024-07-24 22:17:03 -0700

    Update case

--- case/case.py
+++ case/case.py
@@ -11,14 +11,14 @@ from ocp_vscode import *
 # Enclosure dimensions
 width = 215.4
 height = 75.4
-length = 4 * IN
+length = 3.5 * IN
 
 # Window dimensions
 window_width = 190
 window_height = 50
 
 # ledge height
-ledge = 1 / 2 * IN
+# ledge = 1 / 2 * IN
 
 # Enclosure wall thickness
 wall_thickness = 3 / 16 * IN  # 1/4 inch in mm
@@ -28,22 +28,21 @@ inner_width = width - 2 * wall_thickness
 inner_height = height - 2 * wall_thickness
 inner_length = length - 2 * wall_thickness
 
-# Radii
+# Radii for enclosure fillets
 outer_radius = 10
 inner_radius = 5.25
 window_radius = 3.5
 
-# Create the box
+# Create the main box for the enclosure
 box = Box(width, length, height)
 
-# Create the cuttout box (inner box)
-# cuttout_box = Pos(0, -4.5, 0) * Box(inner_width, length - 15, inner_height)
-# cuttout_box = Pos(0, -7, 0) * Box(inner_width, length - 20, inner_height)
+# Create the cuttout box (inner box) for the enclosure
 cuttout_box = Pos(0, -6, 0) * Box(inner_width, length - 22, inner_height)
 
 # Subtract the PCB box from the outer box to create the enclosure walls
 enclosure = box - cuttout_box
 
+# Fillet for inner and outer parts of the enclosure.
 enclosure = fillet(
     itemgetter(0, 1, 2, 3)(enclosure.edges().filter_by(Axis.Y)), radius=outer_radius
 )
@@ -51,22 +50,21 @@ enclosure = fillet(
     itemgetter(8, 9, 10, 11)(enclosure.edges().filter_by(Axis.Y)), radius=inner_radius
 )
 
+# Window cuttout
 window = Pos(0, -length / 2, 0) * Rectangle(
     window_width, window_height, rotation=(90, 0, 0)
 )
 window = fillet(window.vertices(), radius=window_radius)
-
-# Cutt out the window
 enclosure -= extrude(window, amount=5, dir=(0, 1, 0))
 
+# Rear panel cuttout
 rear_panel = Pos(0, length / 2, 0) * Rectangle(
     inner_width, inner_height, rotation=(90, 0, 0)
 )
 rear_panel = fillet(rear_panel.vertices(), radius=inner_radius)
-
-# Cutt out rear panel
 enclosure -= extrude(rear_panel, amount=6, dir=(0, -1, 0))
 
+# Inner rear cuttout
 rear_cuttout_pts = [
     (inner_width / 2, inner_height / 2 - 10),
     (inner_width / 2 - 10, inner_height / 2 - 10),
@@ -81,25 +79,18 @@ rear_cuttout_pts = [
     (inner_width / 2 - 10, -inner_height / 2 + 10),
     (inner_width / 2, -inner_height / 2 + 10),
 ]
-
 l1 = Polyline(*rear_cuttout_pts)
 l2 = Line(l1 @ 0, l1 @ 1)
 line = l1 + l2
 face = Pos(0, length / 2 - 6, 0) * make_face(line).rotate(Axis.X, 90)
 face = fillet(itemgetter(1, 4, 7, 10)(face.vertices()), radius=3)
-
 enclosure -= extrude(face, amount=11, dir=(0, -1, 0))
 
-front_panel = Pos(0, -length / 2 + 5, 0) * Rectangle(
-    inner_width + -1 / 8 * IN, inner_height - 1 / 8 * IN, rotation=(90, 0, 0)
-)
-front_panel = fillet(front_panel.vertices(), radius=inner_radius)
-front_panel = extrude(front_panel, 3, dir=(0, 1, 0))
-
-show(enclosure, front_panel)
 # %%
+# Clock model
 clock = import_step("clock.step")
 
+# IN-12 Nixie tubes
 tube_pts = [
     (10.05, 0, 9.5),
     (44.1, 0, 9.5),
@@ -111,6 +102,7 @@ tube_pts = [
 tube = import_step("IN-12-NixieTube.STEP")
 tubes = [Pos(pos) * tube for pos in tube_pts]
 
+# Nixie tube socket pins
 importer = Mesher()
 socket_pts = [
     # Middle Row
@@ -196,6 +188,7 @@ socket_pts = [
 socket = importer.read("SocketPin.stl")[0]
 sockets = [Pos(pos) * socket for pos in socket_pts]
 
+# INS-1 indicator tubes
 indicator_pts = [
     (27.05, 6, 13),
     (27.05, -6, 13),
@@ -205,42 +198,87 @@ indicator_pts = [
 indicator = import_step("INS1.STEP").rotate(Axis.X, 90).rotate(Axis.Z, 90)
 indicators = [Pos(pos) * indicator for pos in indicator_pts]
 
-# clock = Pos(0, -17.75, 0) * clock.rotate(Axis.X, 90)
+# Assemble the board
 clock = clock.rotate(Axis.X, 90)
 tubes = [tube.rotate(Axis.X, 90) for tube in tubes]
 sockets = [socket.rotate(Axis.X, 90) for socket in sockets]
 indicators = [indicator.rotate(Axis.X, 90) for indicator in indicators]
-board = Pos(0, -17.75, 0) * Compound(children=[*sockets, clock, *tubes, *indicators])
+board = Pos(0, -15, 0) * Compound(children=[*sockets, clock, *tubes, *indicators])
 
-converter = Pos(0, 20, -25) * import_step("converter.step").rotate(Axis.Z, 90)
+# High Voltage FLyback Converter model
+converter = Pos(0, 14.5, -25) * import_step("converter.step").rotate(Axis.Z, 90)
 
-riser1 = Pos(inner_width / 2 - 20, -19, -inner_height / 2 + (5 / 16 * IN) / 2) * Box(
-    10, 10, 5 / 16 * IN
-)
-riser1 = fillet(riser1.edges().filter_by(Axis.Z), radius=2)
-riser2 = Pos(-inner_width / 2 + 20, -19, -inner_height / 2 + (5 / 16 * IN) / 2) * Box(
-    10, 10, 5 / 16 * IN
-)
-riser2 = fillet(riser2.edges().filter_by(Axis.Z), radius=2)
+# Risers
+riser_pts = [
+    (inner_width / 2 - 20, -16, -inner_height / 2 + (5 / 16 * IN) / 2),
+    (-inner_width / 2 + 20, -16, -inner_height / 2 + (5 / 16 * IN) / 2),
+    (37.34 / 2, 14.5 + 16, -inner_height / 2 + (5 / 16 * IN) / 2),
+    (-37.34 / 2, 14.5 + 16, -inner_height / 2 + (5 / 16 * IN) / 2),
+    (-37.34 / 2, 14.5 - 16, -inner_height / 2 + (5 / 16 * IN) / 2),
+    (37.34 / 2, 14.5 - 16, -inner_height / 2 + (5 / 16 * IN) / 2),
+]
+riser = Box(10, 10, 5 / 16 * IN)
+risers = [Pos(pos) * riser for pos in riser_pts]
+risers = [fillet(riser.edges().filter_by(Axis.Z), radius=2) for riser in risers]
 
-front_panel = Pos(0, -length / 2 + 6.5, 0) * Rectangle(
-    window_width + 1 / 4 * IN, window_height + 1 / 4 * IN, rotation=(90, 0, 0)
-)
-front_panel = fillet(window.vertices(), radius=inner_radius)
+# Front panel
+front_panel = Rectangle(inner_width + -1 / 8 * IN, inner_height - 1 / 8 * IN)
+front_panel = fillet(front_panel.vertices(), radius=inner_radius)
+indicator_circle = Circle(7.5 / 2)
+front_panel -= [
+    Pos(pos) * indicator_circle
+    for pos in [
+        (27.05, 6),
+        (27.05, -6),
+        (-27.05, 6),
+        (-27.05, -6),
+    ]
+]
+arc1 = ThreePointArc((-21, 11.5), (-21 / 2, 15.75), (0, 12.5))
+arc2 = ThreePointArc((21, -11.5), (21 / 2, -15.75), (0, -12.5))
+arc3 = ThreePointArc((21, 11.5), (21 / 2, 15.75), (0, 12.5))
+arc4 = ThreePointArc((-21, -11.5), (-21 / 2, -15.75), (0, -12.5))
+l1 = Line((-21, 11.5), (-21, -11.5))
+l2 = Line((21, 11.5), (21, -11.5))
+tube_cuttout = make_face([arc1, arc2, arc3, arc4, l1, l2])
+front_panel -= [
+    Pos(pos) * tube_cuttout
+    for pos in [
+        (0, 0),
+        (44.1 + 10.05, 0),
+        (-44.1 - 10.05, 0),
+    ]
+]
+m3_hole = Circle(3.25 / 2)
+front_panel -= [
+    Pos(pos) * m3_hole
+    for pos in [
+        (inner_width / 2 - 1 / 4 * IN, inner_height / 2 - 1 / 4 * IN),
+        (-inner_width / 2 + 1 / 4 * IN, inner_height / 2 - 1 / 4 * IN),
+        (-inner_width / 2 + 1 / 4 * IN, -inner_height / 2 + 1 / 4 * IN),
+        (inner_width / 2 - 1 / 4 * IN, -inner_height / 2 + 1 / 4 * IN),
+    ]
+]
+front_panel = Pos(0, -length / 2 + 5, 0) * front_panel.rotate(Axis.X, 90)
 front_panel = extrude(front_panel, 3, dir=(0, 1, 0))
 
+# Motion Sensor cuttouts
+motion_pts = [
+    (-inner_width / 2 - wall_thickness / 2, 0, 0),
+    (inner_width / 2 + wall_thickness / 2, 0, 0),
+]
+motion_sensor = Cylinder(radius=11 / 2, height=wall_thickness).rotate(Axis.Y, 90)
+motion_sensor = [Pos(pos) * motion_sensor for pos in motion_pts]
+enclosure -= motion_sensor
 
-show(board, riser1, riser2, enclosure, converter, front_panel)
+# M3 heat set inserts
+# m3_short = import_step("M3_Short.step")
+# m3_standard = import_step("M3_Standard.step")
 
+# Speaker model
+speaker = Pos(-14, -15, inner_height / 2) * import_step("Speaker.STEP").rotate(
+    Axis.X, 90
+)
 
-# standoff_pts = [
-#     (57, L / 2 - 2 * t, -H / 2 + 2 * t),
-#     (-5.621, L / 2 - 2 * t, -H / 2 + 2 * t),
-#     (-5.621, L / 2 - 2 * t - 116.977, -H / 2 + 2 * t),
-#     (57, L / 2 - 2 * t - 116.977, -H / 2 + 2 * t),
-#     (-42, -6 - 4.25 * IN / 2, -H / 2 + 2 * t),
-# ]
-# standoff = Box(2 * t, 2 * t, 2 * t) - Pos(0, 0, t / 2) * Cylinder(
-#     radius=3.25 / 2, height=t
-# )
-# standoffs = [Pos(pos) * standoff for pos in standoff_pts]
+# show(board, risers, enclosure, converter, front_panel, speaker)
+show(board, risers, enclosure, front_panel, converter)
